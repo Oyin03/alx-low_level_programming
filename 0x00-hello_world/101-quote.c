503bebd586de8361bd0a9c9e763f68e36539e3a0
esther <oguntoyinboesther051@gmail.com>
2023-03-20 13:30:07 -0700

    my last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
  * main - Entry
  *
- * Return: Always 2 (sucess)
+ * Return: Always 1 (sucess)
  */
 int main(void)
 {
 
-	put(2,
-	"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (2);
+	Fputs("and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+	return (1);
 }
